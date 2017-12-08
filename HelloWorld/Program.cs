ae7dd685c45134b436e1c41099ee2359bfb57648
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-08 11:57:55 -0700

    UVS

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,10 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello");
+            Console.WriteLine("Write 2 numbers");
+            string number1 = Console.ReadLine();
+            string number2 = Console.ReadLine();
+            Console.WriteLine(number1 + " " + number2);
         }
     }
 }
