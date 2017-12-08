a2ce69e68b63a067afbec8d6614e14111dae5062
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-08 13:16:55 -0700

    UVS

--- HelloWorldiOS/ViewController.designer.cs
+++ HelloWorldiOS/ViewController.designer.cs
@@ -1,17 +1,86 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace HelloWorldiOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton AddButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton DivideButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton MultiplyButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SubtractButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel txtAnswer { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtnumb1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtnumb2 { get; set; }
+
+        [Action ("UIButton398_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton398_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
         {
+            if (AddButton != null) {
+                AddButton.Dispose ();
+                AddButton = null;
+            }
+
+            if (DivideButton != null) {
+                DivideButton.Dispose ();
+                DivideButton = null;
+            }
+
+            if (MultiplyButton != null) {
+                MultiplyButton.Dispose ();
+                MultiplyButton = null;
+            }
+
+            if (SubtractButton != null) {
+                SubtractButton.Dispose ();
+                SubtractButton = null;
+            }
+
+            if (txtAnswer != null) {
+                txtAnswer.Dispose ();
+                txtAnswer = null;
+            }
+
+            if (txtnumb1 != null) {
+                txtnumb1.Dispose ();
+                txtnumb1 = null;
+            }
+
+            if (txtnumb2 != null) {
+                txtnumb2.Dispose ();
+                txtnumb2 = null;
+            }
         }
     }
 }
